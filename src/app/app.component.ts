2b79b6ece7fc7e0dbc04116e377389d3459e143e
ericksuarlim <60761434+ericksuarlim@users.noreply.github.com>
2022-05-13 12:49:42 -0400

    Deploy Desarrollo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
+import * as firebase from 'firebase/app'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'terminal-web-app';
+
+  async ngOnInit(){
+    await this.initFirebase()
+  }
+
+  async initFirebase(){
+    await firebase.initializeApp(environment.firebaseConfig);
+
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDw3pM8qh0vYHQvwJRriZtbkSNUSin0HQQ",
+    authDomain: "tsa-ui-dev-c29b6.firebaseapp.com",
+    projectId: "tsa-ui-dev-c29b6",
+    storageBucket: "tsa-ui-dev-c29b6.appspot.com",
+    messagingSenderId: "333512153438",
+    appId: "1:333512153438:web:488f9635d90b1ebd8cd7cf",
+    measurementId: "G-2V0RLHWW9V"
+  }
 };
 
 /*
