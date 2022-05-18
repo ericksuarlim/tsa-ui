341b3205902d1f28305e803d0be732303aa40c68
ericksuarlim <60761434+ericksuarlim@users.noreply.github.com>
2022-05-17 22:24:55 -0400

    Deploy ui produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBLLBdANHjTH9vlH5B5csJvERi87bOYQHk",
+    authDomain: "tsa-ui-prod.firebaseapp.com",
+    projectId: "tsa-ui-prod",
+    storageBucket: "tsa-ui-prod.appspot.com",
+    messagingSenderId: "861158725365",
+    appId: "1:861158725365:web:218493fe1a91812d0757bc",
+    measurementId: "G-WYZXGH7J4Z"
+  }
 };
